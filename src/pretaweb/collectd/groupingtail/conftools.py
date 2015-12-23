9c4ca159b6e75a06219285773ddd5aa37f864e90
genis ortiz <genis.ortiz.gomis@gmail.com>
2015-12-23 17:38:18 +0100

    Added Total Throughput metric, comments and configs

--- src/pretaweb/collectd/groupingtail/conftools.py
+++ src/pretaweb/collectd/groupingtail/conftools.py
@@ -1,6 +1,6 @@
 import logging
 from groupingtail import GroupingTail
-from instruments import NUM32, CounterInc, CounterSum, GaugeInt, DeriveCounter, GaugeThroughput
+from instruments import NUM32, CounterInc, CounterSum, GaugeInt, DeriveCounter, GaugeThroughput, GaugeTotalThroughput
 
 _getConfFirstValue_NOVAL = object()
 
@@ -16,20 +16,6 @@ def getConfFirstValue(ob, key, default=_getConfFirstValue_NOVAL):
     return default
 
 
-# def getConfAllValues(ob, key, default=_getConfFirstValue_NOVAL):
-#     values = list()
-#     for o in ob.children:
-#         if o.key.lower() == key.lower():
-#             values.append(o.values[0])
-#     if default == _getConfFirstValue_NOVAL:
-#         raise KeyError()
-#     if len(values) == 0:
-#         values.append(default)
-#
-#     logger.info("groupingtail.conftools.getConfAllValues %s\n" % (values))
-#     return values
-
-
 def getConfChildren(ob, key):
     children = []
     for o in ob.children:
@@ -42,44 +28,67 @@ def getConfChildren(ob, key):
 # Instruments library and generators
 #
 def configure_counterinc(conf):
+    # Regex to do the matching
     regex = getConfFirstValue(conf, 'Regex')
-    logger.info("groupingtail.conftools.configure_counterinc %s\n" % (regex))
     return CounterInc(regex)
 
 
 def configure_countersumint(conf):
+    # Regex to do the matching
     regex = getConfFirstValue(conf, "Regex")
+    # Regex group name, or None that acts as first group in regex
     groupname = getConfFirstValue(conf, "GroupName", None)
+    # Function to perform casting to data extracted from field groupname of regex
     value_cast = (lambda x: int(x) % NUM32)
     return CounterSum(regex, value_cast=value_cast, groupname=groupname)
 
 
 def configure_gaugeint(conf):
+    # Regex to do the matching
     regex = getConfFirstValue(conf, "Regex")
+    # Regex group name, or None that acts as first group in regex
     groupname = getConfFirstValue(conf, "GroupName", None)
+    # Function to perform casting to data extracted from field groupname of regex
     value_cast = (lambda x: int(x) % NUM32)
     return GaugeInt(regex, value_cast=value_cast, groupname=groupname)
 
 
 def configure_derivecounter(conf):
+    # Regex to do the matching
     regex = getConfFirstValue(conf, "Regex")
+    # Regex group name, or None that acts as first group in regex
     groupname = getConfFirstValue(conf, "GroupName", None)
+    # Function to perform casting to data extracted from field groupname of regex
     value_cast = (lambda x: int(x) % NUM32)
     return DeriveCounter(regex, value_cast=value_cast, groupname=groupname)
 
 
 def configure_gaugethroughput(conf):
+    # Regex to do the matching
     regex = getConfFirstValue(conf, "Regex")
+    # Regex group name, or None that acts as first group in regex
     groupname = getConfFirstValue(conf, "GroupName", None)
+    # Regex group name, or None that acts as first group in regex
     grouptime = getConfFirstValue(conf, "GroupTime", None)
     return GaugeThroughput(regex, groupname=groupname, grouptime=grouptime)
 
 
+def configure_gaugetotalthroughput(conf):
+    # Regex to do the matching
+    regex = getConfFirstValue(conf, "Regex")
+    groupone = getConfFirstValue(conf, "GroupOne", None)
+    groupother = getConfFirstValue(conf, "GroupOther", None)
+    grouptime = getConfFirstValue(conf, "GroupTime", None)
+    return GaugeTotalThroughput(regex, groupone=groupone, groupother=groupother, grouptime=grouptime)
+
+
+# Dict with configurations of each instrument available
 INSTRUMENTS = {
     "CounterInc": configure_counterinc,
     "CounterSumInt": configure_countersumint,
     "GaugeInt": configure_gaugeint,
     "GaugeThroughput": configure_gaugethroughput,
+    "GaugeTotalThroughput": configure_gaugetotalthroughput,
     "DeriveCounter": configure_derivecounter
 }
 
@@ -87,30 +96,42 @@ INSTRUMENTS = {
 def read_config(conf):
     files = []
     logger.info("groupingtail.conftools.read_config loop\n")
+    # Read all <file>*</file> blocks in config file
     for f in getConfChildren(conf, "File"):
         instance_name = getConfFirstValue(f, 'Instance')
         filepath = f.values[0]
+
+        # Regex to do grouping
         groupby = getConfFirstValue(f, 'GroupBy')
+        # Regex group name, or None that acts as first group in regex
         groupbygroup = getConfFirstValue(f, 'GroupName', None)
+        # Maximum number of groups
         maxgroups = int(getConfFirstValue(f, 'MaxGroups', 64))
 
         logger.info("groupingtail.conftools.read_config filepath %s groupby %s groupbygroup %s\n" % (
             filepath, groupby, groupbygroup))
         gt = GroupingTail(filepath, groupby, groupbygroup)
 
+        # List with files to check
         files.append(dict(
             instance_name=instance_name,
             grouping_tail=gt
         ))
 
+        # For every matching in the file
         for m in getConfChildren(f, 'Match'):
+            # Maching name
             minstance_name = getConfFirstValue(m, "Instance")
+            # Maching type
             valuetype = getConfFirstValue(m, "Type")
 
-            # dstype determins the instrument used
+            # dstype determines the instrument used
             dstype = getConfFirstValue(m, "DSType")
+
+            # read and create instrument
             instrument = INSTRUMENTS[dstype](m)
             logger.info("groupingtail.conftools.read_config instance_name %s valuetype %s instrument %s\n" % (
                 minstance_name, valuetype, instrument))
+            # Add matching to groupingtail
             gt.add_match(minstance_name, valuetype, instrument)
     return files
--- src/pretaweb/collectd/groupingtail/instruments.py
+++ src/pretaweb/collectd/groupingtail/instruments.py
@@ -3,84 +3,84 @@ from datetime import datetime
 import time
 import logging
 import logging.handlers
-from collections import defaultdict
 
 logger = logging.getLogger("GROUPINGTAIL")
 
 NUM32 = 2 ** 32
 
 
+# Parser parent class
 class Instrument(object):
     def __init__(self, regex, maxgroups=64, value_cast=float, groupname=None):
         logger.info("groupingtail.instruments.constructor groupname %s regex %s\n" % (groupname, regex))
 
+        # Compiled regex
         self.test = re.compile(regex)
+        # Maxim number of groups
         self.maxgroups = maxgroups
+        # Cast function
         self.value_cast = value_cast
+        # Regex groupname
         self.regex_group = groupname
 
+        # Data collected and processed
         self.data = None
+        # Groups timestamps
         self.groups = None
+
+        # Reset data and groups structures
         self.reset()
 
+    # Empty bucket and start again
     def reset(self):
-        # Empty bucket and start again
         self.data = {}
         self.groups = {}
 
+    # Mark groups tstamp
     def touch_group(self, groupname):
         self.groups[groupname] = datetime.now()
 
+    # Trim groups which haven't been touched
     def trim_groups(self):
-        # Trim groups which haven't been touched
         items = self.groups.items()
         items.sort()
         self.groups = dict(items[:self.maxgroups])
 
+    # Create newdata with only the members of self.groups and wrap around large integers
     def normalise(self):
-        # Create newdata with only the members of self.groups and wrap around large integers
         newdata = {}
-        logger.info("groupingtail.instruments.normalise group.keys %s\n" % (self.groups.keys()))
         for groupname in self.groups.keys():
             newdata[groupname] = self.value_cast(self.data[groupname])
-            logger.info("groupingtail.instruments.normalise keys %s data %s normalized %s\n" % (
-            self.groups.keys(), self.data[groupname], newdata[groupname]))
         self.data = newdata
 
+    # Return the current results of the bucket
     def read(self):
-        # Return the current results of the bucket
-        logger.info("groupingtail.instruments.read regex_group %s groups %s\n" % (self.regex_group, self.groups))
         self.trim_groups()
-        logger.info("groupingtail.instruments.read trim data %s\n" % (self.data.items()))
         self.normalise()
-        logger.info("groupingtail.instruments.read normalise data %s\n" % (self.data.items()))
         return self.data.items()
 
+    # Do actual data analysis from line
     def append_data(self, groupname, line, mo):
-        # do actual data analysis
-        logger.info("groupingtail.instruments.append_data ERROR mo %s groupname %s line %s\n" % (mo, groupname, line))
         raise NotImplementedError()
 
+    # Performs matching over line line and do proper analysis
     def write(self, groupname, line):
         # analise log line
         mo = self.test.match(line)
-        logger.info("groupingtail.instruments.write mo %s groupname %s line %s\n" % (mo, groupname, line))
+        logger.info("groupingtail.instruments.instrument.write regex %s line %s\n", self.test.pattern, line)
+        # If matching
         if mo is not None:
             try:
-                logger.info(
-                    "groupingtail.instruments.write.append_data mo %s groupname %s line %s\n" % (mo, groupname, line))
+                # Perform analysis
                 self.append_data(groupname, line, mo)
             except ValueError:
-                logger.info("groupingtail.instruments.write ERROR groupname %s line %s\n" % (groupname, line))
+                # Contemplated error
                 pass
             except Exception as e:
                 # the instrument failed.
-                logger.info(
-                    "groupingtail.instruments.write instrument failed groupname %s regex_group %s Exception %s\n" % (
-                    groupname, self.regex_group, e))
                 self.reset()
             else:
-                logger.info("groupingtail.instruments.write touch group groupname %s line %s\n" % (groupname, line))
+                # Mark updated group
                 self.touch_group(groupname)
 
 
@@ -89,47 +89,19 @@ class GaugeInt(Instrument):
         kwargs["value_cast"] = (lambda x: int(x) % NUM32)
         super(GaugeInt, self).__init__(*args, **kwargs)
 
-    def reset(self):
-        super(GaugeInt, self).reset()
-        self.data = defaultdict(list)
-        self.groups = {}
-
     def read(self):
-        logger.info("groupingtail.instruments.gaugeint.read before super\n")
         data_list = super(GaugeInt, self).read()
-        logger.info("groupingtail.instruments.gaugeint.read values %s\n", data_list)
-
-        samples = []
-        for groupname, values_list in data_list:
-            for value in values_list:
-                samples.append((groupname, value))
 
         self.reset()
-        logger.info("groupingtail.instruments.gaugeint.read samples %s data %s\n", samples, self.data.items())
-        return samples
-
-    def normalise(self):
-        # Create newdata with only the members of self.groups and wrap around large integers
-        newdata = defaultdict(list)
-        logger.info("groupingtail.instruments.gaugeint.normalise group.keys %s\n" % (self.groups.keys()))
-        for groupname in self.groups.keys():
-            sample_list = list()
-            for sample in self.data[groupname]:
-                 sample_list.append(self.value_cast(sample))
-            newdata[groupname] = sample_list
-            logger.info("groupingtail.instruments.gaugeint.normalise keys %s data %s normalized %s\n" % (
-            self.groups.keys(), self.data[groupname], newdata[groupname]))
-        self.data = newdata
+        return data_list
 
     def append_data(self, groupname, line, mo):
         if self.regex_group:
             value = self.value_cast(mo.groupdict().get(self.regex_group))
         else:
-            logger.info("groupingtail.instruments.gaugeint.append_data no regex_group line %s\n", line)
             value = self.value_cast(mo.groups()[0])
-        logger.info("groupingtail.instruments.gaugeint.append_data regex_group %s data %s value %s line %s\n",
-                    self.regex_group, self.data.items(), value, line)
-        self.data[groupname].append(value)
+
+        self.data[groupname] = self.data.get(groupname, 0) + value
 
 
 class GaugeThroughput(Instrument):
@@ -141,29 +113,24 @@ class GaugeThroughput(Instrument):
         super(GaugeThroughput, self).__init__(*args, **kwargs)
 
     def read(self):
-        logger.info("groupingtail.instruments.gaugethroughput.read before super\n")
         data = super(GaugeThroughput, self).read()
 
         self.reset()
-        logger.info("groupingtail.instruments.gaugethroughput.read samples %s data %s\n", data, self.data.items())
         return data
 
     def append_data(self, groupname, line, mo):
-        value = self.value_cast(mo.groupdict().get(self.regex_group))
-        elapsed = self.value_cast(mo.groupdict().get(self.grouptime))
+        if self.regex_group is not None and self.grouptime is not None:
+            value = self.value_cast(mo.groupdict().get(self.regex_group))
+            elapsed = self.value_cast(mo.groupdict().get(self.grouptime))
 
-        logger.info("groupingtail.instruments.gaugethroughput.append_data regex_group %s value %s elapsed %s line %s\n",
-                    self.regex_group, value, elapsed, line)
-
-        current = self.data.get(groupname, [0, 0])
-        current[self.value_index] += value
-        current[self.elapsed_index] += elapsed
-        self.data[groupname] = current
+            current = self.data.get(groupname, [0, 0])
+            current[self.value_index] += value
+            current[self.elapsed_index] += elapsed
+            self.data[groupname] = current
 
     def normalise(self):
         # Create newdata with only the members of self.groups and wrap around large integers
         newdata = {}
-        logger.info("groupingtail.instruments.gaugethroughput.normalise group.keys %s\n" % (self.groups.keys()))
         for groupname in self.groups.keys():
             sample_list = self.data[groupname]
             value = sample_list[self.value_index]
@@ -173,9 +140,33 @@ class GaugeThroughput(Instrument):
             else:
                 bw = value
             newdata[groupname] = bw
-            logger.info("groupingtail.instruments.gaugethroughput.normalise keys %s data %s normalized %s\n" % (
-                self.groups.keys(), self.data[groupname], newdata[groupname]))
         self.data = newdata
+
+
+class GaugeTotalThroughput(GaugeThroughput):
+    def __init__(self, *args, **kwargs):
+        self.groupone = kwargs["groupone"]
+        self.groupother = kwargs["groupother"]
+        del kwargs["groupone"]
+        del kwargs["groupother"]
+        super(GaugeTotalThroughput, self).__init__(*args, **kwargs)
+
+    def append_data(self, groupname, line, mo):
+        elapsed = self.value_cast(mo.groupdict().get(self.grouptime))
+        value = None
+        try:
+            value = self.value_cast(mo.groupdict().get(self.groupone))
+        except:
+            try:
+                value = self.value_cast(mo.groupdict().get(self.groupother))
+            except:
+                pass
+
+        if value is not None:
+            current = self.data.get(groupname, [0, 0])
+            current[self.value_index] += value
+            current[self.elapsed_index] += elapsed
+            self.data[groupname] = current
 
 
 class CounterInc(Instrument):
@@ -184,8 +175,6 @@ class CounterInc(Instrument):
         super(CounterInc, self).__init__(*args, **kwargs)
 
     def append_data(self, groupname, line, mo):
-        logger.info("groupingtail.instruments.counterinc.append_data data %s line %s\n", self.data.get(groupname, 0),
-                    line)
         self.data[groupname] = self.data.get(groupname, 0) + 1
 
     def read(self):
@@ -234,12 +223,10 @@ class DeriveCounter(CounterSum):
         self.last_read = now
         if elapsed <= 0:
             elapsed = 1.0
-        logger.info("groupingtail.instruments.DeriveCounter.read last %s now %s elapsed %f\n", str(self.last_read), str(now), elapsed)
         data_list = super(DeriveCounter, self).read()
 
         samples = []
         for groupname, value in data_list:
             samples.append((groupname, value/elapsed))
 
-        logger.info("groupingtail.instruments.DeriveCounter.read samples %s data %s\n", samples, self.data.items())
         return samples
